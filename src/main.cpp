ab7e4966e7a8e1a9f7c01109dc6f7d808a7df401
ViniGambini <vincentgoulet15@gmail.com>
2023-03-15 17:50:25 -0400

    Programme du prof fonctionnel!!

--- src/main.cpp
+++ src/main.cpp
@@ -2,31 +2,27 @@
 
 #define ADDRESSE_I2C_PAR_DEFAUT 0x51
 
-static BufferedSerial serial_port(USBTX, USBRX);
-
-I2C i2c(p28, p27);
+I2C i2c(I2C_SDA, I2C_SCL);
 
 int main() {
 
-  serial_port.set_baud(9600);
   const int addr8bit = ADDRESSE_I2C_PAR_DEFAUT << 1;
 
   char cmd[1];
   char message[10];
 
   while(1) {
-    //printf("test\r\n");
-    cmd[0] = 126; // Démarrer le moteur
+    cmd[0] = 127; // Démarrer le moteur
     i2c.write(addr8bit, cmd, 1);
     ThisThread::sleep_for(100ms);
     cmd[0] = 45; // Envoyer le moteur à la position 45°
     i2c.write(addr8bit, cmd, 1);
+    
+    ThisThread::sleep_for(1000ms);
 
-    ThisThread::sleep_for(500ms);
+    i2c.read(addr8bit, message, sizeof(message));
+    printf("Message reçu: %s", message);
 
-    //i2c.read(addr8bit, message, sizeof(message));
-    //printf("Message reçu: %s\r\n", message);
-
-    //ThisThread::sleep_for(1000ms);
+    ThisThread::sleep_for(1000ms);
   }
 }
